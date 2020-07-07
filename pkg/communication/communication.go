9445fa715789bde1d36db538483a67d72e921b49
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-07 14:48:02 +0200

    added /api/v0/authenticate route

--- pkg/communication/communication.go
+++ pkg/communication/communication.go
@@ -2,16 +2,19 @@ package communication
 
 import (
 	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-playground/validator/v10"
 	"github.com/jmoiron/sqlx"
+	"github.com/ppwfx/user-svc/pkg/business"
 	"github.com/ppwfx/user-svc/pkg/persistence"
 	"github.com/ppwfx/user-svc/pkg/types"
 	"log"
 	"net"
 	"net/http"
+	"time"
 )
 
-func Serve(v *validator.Validate, db *sqlx.DB, addr string) (err error) {
+func Serve(v *validator.Validate, db *sqlx.DB, addr string, hmacSecret string, salt string) (err error) {
 	m := http.NewServeMux()
 
 	m.HandleFunc(types.RouteCreateUser, func(w http.ResponseWriter, r *http.Request) {
@@ -47,7 +50,7 @@ func Serve(v *validator.Validate, db *sqlx.DB, addr string) (err error) {
 
 		err = persistence.InsertUser(db, types.UserModel{
 			Email:    req.Email,
-			Password: req.Password,
+			Password: business.HashPassword(salt, req.Password),
 			FullName: req.FullName,
 		})
 		if err != nil {
@@ -56,6 +59,72 @@ func Serve(v *validator.Validate, db *sqlx.DB, addr string) (err error) {
 
 			return
 		}
+
+		return
+	})
+
+	m.HandleFunc(types.RouteAuthenticate, func(w http.ResponseWriter, r *http.Request) {
+		rsp := types.AuthenticateResponse{}
+		statusCode := http.StatusOK
+
+		defer func() {
+			w.WriteHeader(statusCode)
+
+			err := json.NewEncoder(w).Encode(&rsp)
+			if err != nil {
+				log.Println(err)
+
+				return
+			}
+		}()
+
+		var req types.AuthenticateRequest
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			statusCode = http.StatusBadRequest
+
+			return
+		}
+
+		err = v.Struct(&req)
+		if err != nil {
+			rsp.Error = err.Error()
+			statusCode = http.StatusUnprocessableEntity
+
+			return
+		}
+
+		u, err := persistence.GetUserByEmail(db, req.Email)
+		if err != nil {
+			rsp.Error = types.ErrorInvalidCredentials
+			statusCode = http.StatusUnprocessableEntity
+
+			return
+		}
+
+		if business.HashPassword(salt, req.Password) != u.Password {
+			rsp.Error = types.ErrorInvalidCredentials
+			statusCode = http.StatusUnprocessableEntity
+
+			return
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(24 * time.Hour).Unix(),
+			Subject:   u.Email,
+		})
+
+		tokenString, err := token.SignedString([]byte(hmacSecret))
+		if err != nil {
+			log.Println(err)
+
+			rsp.Error = types.ErrorInternalError
+			statusCode = http.StatusInternalServerError
+
+			return
+		}
+
+		rsp.AccessToken = tokenString
 
 		return
 	})
