2a0e0cc6f27f2ce1ebfc8519e788d98e63de63ae
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-07 14:43:59 +0200

    added RouteAuthenticate, ErrorInvalidCredentials, and ErrorInternalError consts

--- pkg/types/const.go
+++ pkg/types/const.go
@@ -1,6 +1,9 @@
 package types
 
 const (
-	RouteCreateUser = "/api/v0/createUser"
-	ContentTypeJson = "application/json"
+	RouteCreateUser         = "/api/v0/createUser"
+	RouteAuthenticate       = "/api/v0/authenticate"
+	ContentTypeJson         = "application/json"
+	ErrorInvalidCredentials = "invalid credentials"
+	ErrorInternalError      = "internal error"
 )
