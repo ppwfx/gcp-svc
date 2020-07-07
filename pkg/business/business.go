c489f9a1779f4154dad08d6c4b53c3b84c748bb6
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-07 14:43:24 +0200

    added HashPassword func

--- pkg/business/business.go
+++ pkg/business/business.go
@@ -1,1 +1,10 @@
 package business
+
+import (
+	"crypto/sha1"
+	"fmt"
+)
+
+func HashPassword(salt string, password string) string {
+	return fmt.Sprintf("%x", sha1.New().Sum([]byte(salt+password)))
+}
