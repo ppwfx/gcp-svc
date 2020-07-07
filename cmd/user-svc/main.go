638056cc4565f32268c0eadf5aeed5550275143c
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-07 11:06:27 +0200

    added main.go to serve http handler

--- cmd/user-svc/main.go
+++ cmd/user-svc/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"flag"
+	"github.com/ppwfx/user-svc/pkg/communication"
+	"github.com/ppwfx/user-svc/pkg/types"
+	"log"
+)
+
+var serveArgs = types.ServeArgs{}
+
+func main() {
+	flag.StringVar(&serveArgs.Addr, "addr", "", "")
+
+	err := communication.Serve(serveArgs.Addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
