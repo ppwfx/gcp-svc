b9a18197f660bda3816163aa3131c59168521a3e
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-07 13:15:25 +0200

    added validation, and persistence

--- cmd/user-svc/main.go
+++ cmd/user-svc/main.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"flag"
+	"github.com/go-playground/validator/v10"
 	"github.com/ppwfx/user-svc/pkg/communication"
+	"github.com/ppwfx/user-svc/pkg/persistence"
 	"github.com/ppwfx/user-svc/pkg/types"
 	"log"
+	"time"
 )
 
-var serveArgs = types.ServeArgs{}
+var args = types.ServeArgs{}
 
 func main() {
-	flag.StringVar(&serveArgs.Addr, "addr", "", "")
-
+	flag.StringVar(&args.Addr, "addr", "", "")
+	flag.StringVar(&args.DbConnection, "db-connection", "", "")
 	flag.Parse()
 
-	err := communication.Serve(serveArgs.Addr)
+	v := validator.New()
+
+	err := func() (err error) {
+		err = persistence.WaitForDb(args.DbConnection)
+		if err != nil {
+			return
+		}
+
+		db, err := persistence.GetDb(25, 25, 5*time.Minute, args.DbConnection)
+		if err != nil {
+			return
+		}
+
+		err = communication.Serve(v, db, args.Addr)
+		if err != nil {
+			return
+		}
+
+		return
+	}()
 	if err != nil {
 		log.Fatal(err)
 	}
--- pkg/communication/communication.go
+++ pkg/communication/communication.go
@@ -2,34 +2,62 @@ package communication
 
 import (
 	"encoding/json"
+	"github.com/go-playground/validator/v10"
+	"github.com/jmoiron/sqlx"
+	"github.com/ppwfx/user-svc/pkg/persistence"
 	"github.com/ppwfx/user-svc/pkg/types"
 	"log"
 	"net"
 	"net/http"
 )
 
-func Serve(addr string) (err error) {
+func Serve(v *validator.Validate, db *sqlx.DB, addr string) (err error) {
 	m := http.NewServeMux()
 
 	m.HandleFunc(types.RouteCreateUser, func(w http.ResponseWriter, r *http.Request) {
+		rsp := types.CreateUserResponse{}
+		statusCode := http.StatusOK
+
+		defer func() {
+			w.WriteHeader(statusCode)
+
+			err := json.NewEncoder(w).Encode(&rsp)
+			if err != nil {
+				log.Println(err)
+
+				return
+			}
+		}()
+
 		var req types.CreateUserRequest
 		err := json.NewDecoder(r.Body).Decode(&req)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			statusCode = http.StatusBadRequest
 
 			return
 		}
 
-		rsp := types.CreateUserResponse{}
-
-		err = json.NewEncoder(w).Encode(&rsp)
+		err = v.Struct(&req)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-
-			log.Println(err)
+			rsp.Error = err.Error()
+			statusCode = http.StatusUnprocessableEntity
 
 			return
 		}
+
+		err = persistence.InsertUser(db, types.UserModel{
+			Email:    req.Email,
+			Password: req.Password,
+			FullName: req.FullName,
+		})
+		if err != nil {
+			rsp.Error = err.Error()
+			statusCode = http.StatusUnprocessableEntity
+
+			return
+		}
+
+		return
 	})
 
 	l, err := net.Listen("tcp", addr)
