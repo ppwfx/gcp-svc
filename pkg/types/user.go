93479bd8ac27ac6a631c0e83f91d63bf83b93e5c
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-07 13:14:22 +0200

    added UserModel

--- pkg/types/user.go
+++ pkg/types/user.go
@@ -1,11 +1,18 @@
 package types
 
 type CreateUserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"email"`
-	FullName string `json:"full_name"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+	FullName string `json:"fullname" validate:"required"`
 }
 
 type CreateUserResponse struct {
 	Error string `json:"email"`
+}
+
+type UserModel struct {
+	Id       int    `db:"id"`
+	Email    string `db:"email"`
+	Password string `db:"password"`
+	FullName string `db:"fullname"`
 }
