788235081be316f1715492fa9351d1699be1a105
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-07 16:02:53 +0200

    added auth related consts

--- pkg/types/const.go
+++ pkg/types/const.go
@@ -2,8 +2,14 @@ package types
 
 const (
 	RouteCreateUser         = "/api/v0/createUser"
+	RouteListUsers          = "/api/v0/listUsers"
 	RouteAuthenticate       = "/api/v0/authenticate"
 	ContentTypeJson         = "application/json"
 	ErrorInvalidCredentials = "invalid credentials"
 	ErrorInternalError      = "internal error"
+	ErrorUnauthorized       = "unauthorized"
+	HeaderAuthorization     = "Authorization"
+	HeaderContentType       = "Content-Type"
+	PrefixBearer            = "Bearer "
+	ClaimSub                = "sub"
 )
