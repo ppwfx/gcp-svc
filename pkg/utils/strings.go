ad2e2695210bb0fe80c0590c5f8c21553f9ce017
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-22 20:42:29 +0200

    added string helper to generate random strings

---
+++ pkg/utils/strings.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(letters))]
+	}
+	return string(b)
+}
