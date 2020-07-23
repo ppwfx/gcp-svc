f08550d199f8755472c1f3ede650ecf02a6523de
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-23 22:15:49 +0200

    added stackdriver compatible log format

--- cmd/user-svc/main.go
+++ cmd/user-svc/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"flag"
+	"github.com/pkg/errors"
 	"github.com/ppwfx/user-svc/pkg/business"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"log"
 	"time"
 
@@ -15,6 +17,34 @@ import (
 )
 
 var args = types.ServeArgs{}
+
+var EncoderConfig = zapcore.EncoderConfig{
+	TimeKey:        "eventTime",
+	LevelKey:       "severity",
+	NameKey:        "logger",
+	CallerKey:      "caller",
+	MessageKey:     "message",
+	StacktraceKey:  "stacktrace",
+	LineEnding:     zapcore.DefaultLineEnding,
+	EncodeLevel:    EncodeLevel,
+	EncodeTime:     zapcore.ISO8601TimeEncoder,
+	EncodeDuration: zapcore.SecondsDurationEncoder,
+	EncodeCaller:   zapcore.ShortCallerEncoder,
+}
+
+var logLevelSeverity = map[zapcore.Level]string{
+	zapcore.DebugLevel:  "DEBUG",
+	zapcore.InfoLevel:   "INFO",
+	zapcore.WarnLevel:   "WARNING",
+	zapcore.ErrorLevel:  "ERROR",
+	zapcore.DPanicLevel: "CRITICAL",
+	zapcore.PanicLevel:  "ALERT",
+	zapcore.FatalLevel:  "EMERGENCY",
+}
+
+func EncodeLevel(lv zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(logLevelSeverity[lv])
+}
 
 func main() {
 	flag.StringVar(&args.Addr, "addr", "", "")
@@ -28,20 +58,38 @@ func main() {
 	err := func() (err error) {
 		db, err := persistence.OpenPostgresDB(25, 25, 5*time.Minute, args.DbConnection)
 		if err != nil {
+			err = errors.Wrap(err, "failed to open postgres")
+
 			return
 		}
 
 		err = persistence.ConnectToPostgresDb(context.Background(), db, 5*time.Second)
 		if err != nil {
+			err = errors.Wrap(err, "failed to connect to postgres")
+
 			return
 		}
 
-		c := zap.NewProductionConfig()
-		c.DisableStacktrace = true
-		l, _ := c.Build()
+		c := &zap.Config{
+			Level:             zap.NewAtomicLevelAt(zapcore.InfoLevel),
+			Encoding:          "json",
+			EncoderConfig:     EncoderConfig,
+			OutputPaths:       []string{"stdout"},
+			ErrorOutputPaths:  []string{"stderr"},
+			DisableStacktrace: true,
+		}
+
+		l, err := c.Build()
+		if err != nil {
+			err = errors.Wrap(err, "failed to build zap logger")
+
+			return
+		}
 
 		err = communication.Serve(v, l.Sugar(), db, args.Addr, args.HmacSecret, args.AllowedSubjectSuffix, business.DefaultArgon2IdOpts)
 		if err != nil {
+			err = errors.Wrap(err, "failed to listen")
+
 			return
 		}
 
