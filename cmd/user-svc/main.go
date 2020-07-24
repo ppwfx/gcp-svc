58b983ea7a18a8bb24299af3efd82db8c42ca1eb
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-07-24 07:51:51 +0200

    flush zap logger buffer before termination

--- cmd/user-svc/main.go
+++ cmd/user-svc/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"context"
 	"flag"
-	"github.com/pkg/errors"
-	"github.com/ppwfx/user-svc/pkg/business"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 	"log"
 	"time"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/pkg/errors"
+	"github.com/ppwfx/user-svc/pkg/business"
 	"github.com/ppwfx/user-svc/pkg/communication"
 	"github.com/ppwfx/user-svc/pkg/persistence"
 	"github.com/ppwfx/user-svc/pkg/types"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var args = types.ServeArgs{}
@@ -53,9 +53,36 @@ func main() {
 	flag.StringVar(&args.AllowedSubjectSuffix, "allowed-subject-suffix", "", "")
 	flag.Parse()
 
+	c := &zap.Config{
+		Level:             zap.NewAtomicLevelAt(zapcore.InfoLevel),
+		Encoding:          "json",
+		EncoderConfig:     EncoderConfig,
+		OutputPaths:       []string{"stdout"},
+		ErrorOutputPaths:  []string{"stderr"},
+		DisableStacktrace: true,
+	}
+
 	v := validator.New()
 
 	err := func() (err error) {
+		l, err := c.Build()
+		if err != nil {
+			err = errors.Wrap(err, "failed to build zap logger")
+
+			return
+		}
+
+		defer func() {
+			err := l.Sync()
+			if err != nil {
+				err = errors.Wrap(err, "failed to flush logger buffer")
+
+				log.Print(err)
+
+				return
+			}
+		}()
+
 		db, err := persistence.OpenPostgresDB(25, 25, 5*time.Minute, args.DbConnection)
 		if err != nil {
 			err = errors.Wrap(err, "failed to open postgres")
@@ -70,22 +97,6 @@ func main() {
 			return
 		}
 
-		c := &zap.Config{
-			Level:             zap.NewAtomicLevelAt(zapcore.InfoLevel),
-			Encoding:          "json",
-			EncoderConfig:     EncoderConfig,
-			OutputPaths:       []string{"stdout"},
-			ErrorOutputPaths:  []string{"stderr"},
-			DisableStacktrace: true,
-		}
-
-		l, err := c.Build()
-		if err != nil {
-			err = errors.Wrap(err, "failed to build zap logger")
-
-			return
-		}
-
 		err = communication.Serve(v, l.Sugar(), db, args.Addr, args.HmacSecret, args.AllowedSubjectSuffix, business.DefaultArgon2IdOpts)
 		if err != nil {
 			err = errors.Wrap(err, "failed to listen")
@@ -96,6 +107,10 @@ func main() {
 		return
 	}()
 	if err != nil {
-		log.Fatal(err)
+		err = errors.Wrap(err, "failed to run service")
+
+		return
 	}
+
+	return
 }
