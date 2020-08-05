34a4c35ce64783a7edc25bf9d5ec9e390aa546fd
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-08-05 16:57:40 +0200

    updated sql

--- pkg/persistence/migrations/000001_create_users_table.up.sql
+++ pkg/persistence/migrations/000001_create_users_table.up.sql
@@ -1,7 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE OR REPLACE FUNCTION set_updated_at()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(256) UNIQUE NOT NULL,
-    password VARCHAR(256) NOT NULL,
-    role VARCHAR(256) NOT NULL,
-    fullname VARCHAR(256) NOT NULL
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    user_group TEXT NOT NULL,
+    fullname TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
+
+CREATE TRIGGER set_updated_at_users
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+EXECUTE PROCEDURE set_updated_at();
